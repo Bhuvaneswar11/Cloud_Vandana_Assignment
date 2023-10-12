39fef2e9bd91b948c3af91fdb2891b300616565f
Bhuvaneswar11 <126778626+Bhuvaneswar11@users.noreply.github.com>
2023-10-12 14:24:56 +0530

    Update Shufflearray.java

--- Shufflearray.java
+++ Shufflearray.java
@@ -1,12 +1,22 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;  
 
-public class Shufflearray {
+public class ShuffleArray {
     public static void main(String[] args) {
-        Integer[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
-        List<Integer> numberList = Arrays.asList(numbers);
-        Collections.shuffle(numberList);
-        System.out.println("Shuffled array: " + numberList);
+        Integer[] numbers = {1, 2, 3, 4, 5, 6, 7};
+        shuffleArray(numbers);
+        System.out.println("Shuffled array: " + Arrays.toString(numbers));
+    }
+
+    public static void shuffleArray(Integer[] array) {
+        int n = array.length;
+        for (int i = n - 1; i > 0; i--) {
+  
+            int randomIndex = (int) (Math.random() * (i + 1));
+
+            
+            int temp = array[i];
+            array[i] = array[randomIndex];
+            array[randomIndex] = temp;
+        }
     }
 }
