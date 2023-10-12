d3264a8aecd38d343eb3a9f5c6436be61d876aa0
Bhuvaneswar11 <126778626+Bhuvaneswar11@users.noreply.github.com>
2023-10-12 22:12:27 +0530

    Update Arraydesc.js

--- Arraydesc.js
+++ Arraydesc.js
@@ -1,7 +1,7 @@
 const numbers =[5,2,8,3,6,7];
 function Descending(arr){
     const n=arr.length;
-    for(let i=0;i<n-i;i++)
+    for(let i=0;i<n-1;i++)
     {
         for(let j=0;j<n-i-1;j++)
         {
