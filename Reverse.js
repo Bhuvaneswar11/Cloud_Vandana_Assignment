ad96fbe65ff0a2b603c503e5e0623b02a424c46f
Bhuvaneswar11 <126778626+Bhuvaneswar11@users.noreply.github.com>
2023-10-12 21:53:12 +0530

    Update Reverse.js

--- Reverse.js
+++ Reverse.js
@@ -1,24 +1,31 @@
-function reverseWordsInSentence(sentence) {
+function reverseWordsInSentence(sentence)
+ {
  let reversedSentence = "";
  let currentWord = "";
 
-for (let i = 0; i < sentence.length; i++) {
-   if (sentence[i] === " " || i === sentence.length- 1) {
-     if (i === sentence.length- 1) {
+for (let i = 0; i < sentence.length; i++)
+ {
+   if (sentence[i] === " " || i === sentence.length- 1) 
+   {
+     if (i === sentence.length- 1) 
+     {
        currentWord += sentence[i];
      }
 
-     for (let j = currentWord.length- 1; j >= 0; j-- ) {
+     for (let j = currentWord.length- 1; j >= 0; j-- )
+      {
        reversedSentence += currentWord[j];
-     }
+      }
 
-     if (i !== sentence.length- 1) {
+     if (i !== sentence.length- 1)
+     {
        reversedSentence += " ";
      }
 
      currentWord = "";
    } 
-   else{
+   else
+   {
      currentWord += sentence[i];
    }
  }
