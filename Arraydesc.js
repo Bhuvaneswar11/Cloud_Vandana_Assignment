20d9e1e6fee4789ae47fd260f41a0954c01fe1d0
Bhuvaneswar11 <126778626+Bhuvaneswar11@users.noreply.github.com>
2023-10-12 21:01:11 +0530

    Update Arraydesc.js

--- Arraydesc.js
+++ Arraydesc.js
@@ -1,18 +1,18 @@
-const numbers = [5, 2, 9, 1, 4, 6];
-
-function bubbleSortDescending(arr) {
-    const n = arr.length;
-    for (let i = 0; i < n - 1; i++) {
-        for (let j = 0; j < n - i - 1; j++) {
-            if (arr[j] < arr[j + 1]) {
-             
-                const temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+const numbers =[5,2,8,3,6,7];
+function Descending(arr){
+    const n=arr.length;
+    for(let i=0;i<n-i;i++)
+    {
+        for(let j=0;j<n-i-1;j++)
+        {
+            if(arr[j]<arr[j+1])
+            {
+                const temp=arr[j];
+                arr[j] =arr[j+1];
+                arr[j+1]=temp;
             }
         }
     }
 }
-
-bubbleSortDescending(numbers);
+Descending(numbers);
 console.log(numbers);
