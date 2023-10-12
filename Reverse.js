12f1eb98ca780441eb3ec53c46b257426b057296
Bhuvaneswar11 <126778626+Bhuvaneswar11@users.noreply.github.com>
2023-10-12 20:41:00 +0530

    Update Reverse.js

--- Reverse.js
+++ Reverse.js
@@ -1,9 +1,32 @@
 function reverseWordsInSentence(sentence) {
-  return sentence
-    .split(" ")
-    .map((word) => word.split("").reverse().join(""))
-    .join(" ");
+ let reversedSentence = "";
+ let currentWord = "";
+
+for (let i = 0; i < sentence.length; i++) {
+   if (sentence[i] === " " || i === sentence.length- 1) {
+     if (i === sentence.length- 1) {
+       currentWord += sentence[i];
+     }
+
+     for (let j = currentWord.length- 1; j >= 0; j-- ) {
+       reversedSentence += currentWord[j];
+     }
+
+     if (i !== sentence.length- 1) {
+       reversedSentence += " ";
+     }
+
+     currentWord = "";
+   } 
+   else{
+     currentWord += sentence[i];
+   }
+ }
+
+ return reversedSentence;
 }
+
 const inputSentence = "Bhuvaneswar is a software developer";
 const reversedSentence = reverseWordsInSentence(inputSentence);
 console.log(reversedSentence);
+
